2cf74f025be48b0234755b4e68de1d750bca2042
rahul soni <sonirahul12342@gmail.com>
2020-07-12 15:28:30 +0530

    cchanges to getting

--- getting.java
+++ getting.java
@@ -1,14 +1,9 @@
 
 class Main{
 public static void  main(String args[])
-<<<<<<< HEAD
 { //master commit
-||||||| merged common ancestors
-{
-=======
 
 { //sidebar commit
->>>>>>> sidebar
    // your code here
    System.out.print("Hello World");
 }
