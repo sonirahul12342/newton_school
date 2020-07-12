41585b51f628b705ee34160583695e061f5b902c
rahul soni <sonirahul12342@gmail.com>
2020-07-12 16:53:02 +0530

    adding code to file

---
+++ naturalnumber.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner k=new Scanner(System.in);
+     int t=k.nextInt();
+     for(int i=0;i<t;i++){
+         int n=k.nextInt();
+         System.out.println(((n+1)*n)/2);
+
+     }
+    
+    }
+}
